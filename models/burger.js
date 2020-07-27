25def6e7dd01bce1fce930b71c6c9f33ab38bbba
Laura Lara <llaraama@uncc.edu>
2020-07-27 15:51:23 -0400

    added the orm.js with the functions

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,22 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-
+    selectALL: function(cb) {
+      orm.selectALL("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols,vals,cb) {
+      orm.create("burgers", cols,vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
   };
   
 
