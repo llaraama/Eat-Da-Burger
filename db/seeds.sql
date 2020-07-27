04c6204840158e66d709bc8e8f35a79207c80fa5
Laura Lara <llaraama@uncc.edu>
2020-07-27 14:46:34 -0400

    setup the app with necessary files and structure

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger',false);
+
+INSERT INTO burgers (name, devoured) VALUES ('Chili Burger',false);
+
+INSERT INTO burgers (name, devoured) VALUES ('Elk Burger',false);
+
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger',false);
+
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger',false);
+
+INSERT INTO burgers (name, devoured) VALUES ('Pizza Burger',false);
+
+
+
