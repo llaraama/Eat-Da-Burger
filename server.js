0c7c91c447755dd32659af64c588b5ba7db073f3
Laura Lara <llaraama@uncc.edu>
2020-07-27 15:03:28 -0400

    filled in the burgers_controller.js for the get, post, and put request

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Packages
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
@@ -7,21 +7,19 @@ var port = process.env.port || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
+
+app.use(methodOverride("_method"));
 
 app.listen(port);
