d0dcce5ede7e0dcd22458bc3b654873aba478b62
Laura Lara <llaraama@uncc.edu>
2020-07-27 21:23:36 -0400

    added necessary steps to deploy to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-  id int NOT NULL AUTO_INCREMENT,
+  id INT AUTO_INCREMENT NOT NULL,
   name varchar(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  PRIMARY KEY (id)
+  createdAt TIMESTAMP NOT NULL,
+  PRIMARY KEY(id)
 );
