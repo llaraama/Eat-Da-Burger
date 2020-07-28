008e14de7f62dad742af3971857291068359e450
Laura Lara <llaraama@uncc.edu>
2020-07-27 22:10:12 -0400

    troubleshooting heroku page

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE IF NOT EXISTS burgers_db;
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
 
 USE burgers_db;
-
-DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers (
   id INT AUTO_INCREMENT NOT NULL,
   name varchar(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  createdAt TIMESTAMP NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY (id)
 );
